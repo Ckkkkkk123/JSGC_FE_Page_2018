44e6f7847e6348e783f2323a249fd59af8250a90
lzqneu <1010366772@qq.com>
2018-07-12 13:56:07 +0800

    projectlzq

--- js/jsxm.js
+++ js/jsxm.js
@@ -3,3 +3,10 @@ function resetInput(){
 }
 
 var url=''
+
+function getDepartmentOption(departmentList){
+	$.each(departmentList, function(index,el) {
+		//alert(el);
+		$('#projectDepartment').append('<option value='+index+'>'+el+'</option>');
+	});
+}
