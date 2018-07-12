585389866874273ccaf104382f3cadecc1643286
mei_eat_bao <2408297044@qq.com>
2018-07-12 17:19:43 +0800

    资产页面

--- js/jsxm.js
+++ js/jsxm.js
@@ -10,3 +10,46 @@ function getDepartmentOption(departmentList){
 		$('#projectDepartment').append('<option value='+index+'>'+el+'</option>');
 	});
 }
+
+function getLendTypeOption(lendTypeList){
+	$.each(lendTypeList, function(index,el) {
+		//alert(el);
+		$('#lendType').append('<option value='+index+'>'+el+'</option>');
+	});
+}
+
+function getBorrowTypeOption(borrowTypeList){
+	$.each(borrowTypeList, function(index,el) {
+		//alert(el);
+		$('#borrowType').append('<option value='+index+'>'+el+'</option>');
+	});
+}
+
+function getPayWayOption(payWayList){
+	$.each(payWayList, function(index,el) {
+		//alert(el);
+		$('#payWay').append('<option value='+index+'>'+el+'</option>');
+	});
+}
+ 
+
+function getPurchaseWayOption(purchaseWayList){
+	$.each(purchaseWayList, function(index,el) {
+		//alert(el);
+		$('#purchaseWay').append('<option value='+index+'>'+el+'</option>');
+	});
+}
+
+function getOrgWayOption(orgWayList){
+	$.each(orgWayList, function(index,el) {
+		//alert(el);
+		$('#orgWay').append('<option value='+index+'>'+el+'</option>');
+	});
+}
+
+function getPurchaseTypeOption(purchaseTypeList){
+	$.each(purchaseTypeList, function(index,el) {
+		//alert(el);
+		$('#purchaseType').append('<option value='+index+'>'+el+'</option>');
+	});
+}
