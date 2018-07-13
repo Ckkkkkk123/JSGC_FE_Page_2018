6b3fa06916f6a1d303fb97157140cd50160f1acc
tongao <tongao713@live.cn>
2018-07-13 15:38:54 +0800

    基本完成 项目列表页 项目信息编辑

--- js/jsxm.js
+++ js/jsxm.js
@@ -1,55 +1,64 @@
-function resetInput(){
+function resetInput() {
 	$(" input").val("")
 }
 
-var url=''
+var baseUrl = "http://127.0.0.1:2333";
+var getProjectListUrl = baseUrl + "/getProjectList";
+var getProjectDetailUrl = baseUrl + "/getProjectDetail";
+var updateProjectDetailUrl = baseUrl + "/updateProjectDetail";
 
-function getDepartmentOption(departmentList){
-	$.each(departmentList, function(index,el) {
+function getDepartmentOption(departmentList) {
+	for(i in departmentList){
+   	 	
+    			$('#projectDepartmentEdit').append('<option value=' + departmentList[i]["departmentID"] + '>' + departmentList[i]["departmentName"] + '</option>');
+    			$('#projectDepartment').append('<option value=' + departmentList[i]["departmentID"] + '>' + departmentList[i]["departmentName"] + '</option>');
+        }
+    }
+	
+//	$.each(departmentList, function(index, el) {
+//		//alert(el);
+//		$('#projectDepartment').append('<option value=' + index + '>' + el + '</option>');
+//	});
+
+
+function getLendTypeOption(lendTypeList) {
+	$.each(lendTypeList, function(index, el) {
 		//alert(el);
-		$('#projectDepartment').append('<option value='+index+'>'+el+'</option>');
+		$('#lendType').append('<option value=' + index + '>' + el + '</option>');
 	});
 }
 
-function getLendTypeOption(lendTypeList){
-	$.each(lendTypeList, function(index,el) {
+function getBorrowTypeOption(borrowTypeList) {
+	$.each(borrowTypeList, function(index, el) {
 		//alert(el);
-		$('#lendType').append('<option value='+index+'>'+el+'</option>');
+		$('#borrowType').append('<option value=' + index + '>' + el + '</option>');
 	});
 }
 
-function getBorrowTypeOption(borrowTypeList){
-	$.each(borrowTypeList, function(index,el) {
+function getPayWayOption(payWayList) {
+	$.each(payWayList, function(index, el) {
 		//alert(el);
-		$('#borrowType').append('<option value='+index+'>'+el+'</option>');
+		$('#payWay').append('<option value=' + index + '>' + el + '</option>');
 	});
 }
 
-function getPayWayOption(payWayList){
-	$.each(payWayList, function(index,el) {
+function getPurchaseWayOption(purchaseWayList) {
+	$.each(purchaseWayList, function(index, el) {
 		//alert(el);
-		$('#payWay').append('<option value='+index+'>'+el+'</option>');
-	});
-}
- 
-
-function getPurchaseWayOption(purchaseWayList){
-	$.each(purchaseWayList, function(index,el) {
-		//alert(el);
-		$('#purchaseWay').append('<option value='+index+'>'+el+'</option>');
+		$('#purchaseWay').append('<option value=' + index + '>' + el + '</option>');
 	});
 }
 
-function getOrgWayOption(orgWayList){
-	$.each(orgWayList, function(index,el) {
+function getOrgWayOption(orgWayList) {
+	$.each(orgWayList, function(index, el) {
 		//alert(el);
-		$('#orgWay').append('<option value='+index+'>'+el+'</option>');
+		$('#orgWay').append('<option value=' + index + '>' + el + '</option>');
 	});
 }
 
-function getPurchaseTypeOption(purchaseTypeList){
-	$.each(purchaseTypeList, function(index,el) {
+function getPurchaseTypeOption(purchaseTypeList) {
+	$.each(purchaseTypeList, function(index, el) {
 		//alert(el);
-		$('#purchaseType').append('<option value='+index+'>'+el+'</option>');
+		$('#purchaseType').append('<option value=' + index + '>' + el + '</option>');
 	});
 }
